56d65c84276b60bbe17e02fd573a195352d920a9
Maria6112 <127786529+Maria6112@users.noreply.github.com>
2025-07-17 16:18:00 +0300

    mobile changes

--- src/components/DownloadResumeButton.js
+++ src/components/DownloadResumeButton.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import "../pages/AboutMe.css";
 const DownloadResumeButton = () => {
   return (
-    <a href="/MariaFominResume.pdf" download className="hire">
-      Download CV
+    <a
+      href="/MariaFominResume.pdf"
+      download
+      className="download-resume-button hire"
+    >
+      <span className="text-label">Download CV</span>
+      <span className="cv-label">CV</span>
       <img src="./images/download.png" alt="download" />
     </a>
   );
